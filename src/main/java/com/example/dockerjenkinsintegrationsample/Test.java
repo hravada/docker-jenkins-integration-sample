973b6c1c593890cd5fd4e5f2744c3e0bc08abb71
AaradhyaRavada <hravada@gmail.com>
2021-01-27 13:32:24 +0530

    second commit

---
+++ src/main/java/com/example/dockerjenkinsintegrationsample/Test.java
@@ -0,0 +1,10 @@
+package com.example.dockerjenkinsintegrationsample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git to Jenkins and docker integration");
+
+	}
+
+}
